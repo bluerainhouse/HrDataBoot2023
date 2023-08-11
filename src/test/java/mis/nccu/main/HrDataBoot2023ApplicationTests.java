dff3e850bed793025bb7effa6f69022e2ac095bc
bluerainhouse <samba255256@gmail.com>
2023-08-11 13:39:07 +0800

    first

---
+++ src/test/java/mis/nccu/main/HrDataBoot2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package mis.nccu.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrDataBoot2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

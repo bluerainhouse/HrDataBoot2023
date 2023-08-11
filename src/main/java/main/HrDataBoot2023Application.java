740b831c2c1af9832bea72595078f9df09ef5dbb
bluerainhouse <samba255256@gmail.com>
2023-08-11 17:32:29 +0800

    half

--- src/main/java/main/HrDataBoot2023Application.java
+++ src/main/java/main/HrDataBoot2023Application.java
@@ -1,9 +1,13 @@
-package mis.nccu.main;
+package main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan(basePackages = {"models"})
 public class HrDataBoot2023Application {
 
 	public static void main(String[] args) {

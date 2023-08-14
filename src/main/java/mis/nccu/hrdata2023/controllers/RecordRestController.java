5655e6002ba41153c7e9ceaedddbd478cce85d5f
bluerainhouse <samba255256@gmail.com>
2023-08-14 16:33:13 +0800

    authenticate half

--- src/main/java/mis/nccu/hrdata2023/controllers/RecordRestController.java
+++ src/main/java/mis/nccu/hrdata2023/controllers/RecordRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,6 +43,7 @@ public class RecordRestController {
 	}
 
 	@GetMapping(value = "/allchart")
+	@PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_MODERATOR') or hasRole('ROLE_ADMIN')")
 	public List<Record> outputJson() {
 		return recordService.getAll();
 	}
